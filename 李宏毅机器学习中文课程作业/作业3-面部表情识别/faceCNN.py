2adca75893c43ccf3a6ef0b6f0ab40f5755417dd
idotc <244979481@qq.com>
2019-10-30 23:37:35 +0800

    Create faceCNN.py

---
+++ 李宏毅机器学习中文课程作业/作业3-面部表情识别/faceCNN.py
@@ -0,0 +1,63 @@
+class FaceCNN(nn.Module):
+    # 初始化网络结构
+    def __init__(self):
+        super(FaceCNN, self).__init__()
+        
+        # 第一次卷积、池化
+        self.conv1 = nn.Sequential(
+            # 输入通道数in_channels，输出通道数(即卷积核的通道数)out_channels，卷积核大小kernel_size，步长stride，对称填0行列数padding
+            # input:(bitch_size, 1, 48, 48), output:(bitch_size, 64, 48, 48), (48-3+2*1)/1+1 = 48
+            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1), # 卷积层
+            nn.BatchNorm2d(num_features=64), # 归一化
+            nn.RReLU(inplace=True), # 激活函数
+            # output(bitch_size, 64, 24, 24)
+            nn.MaxPool2d(kernel_size=2, stride=2), # 最大值池化
+        )
+        
+        # 第二次卷积、池化
+        self.conv2 = nn.Sequential(
+            # input:(bitch_size, 64, 24, 24), output:(bitch_size, 128, 24, 24), (24-3+2*1)/1+1 = 24
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=128),
+            nn.RReLU(inplace=True),
+            # output:(bitch_size, 128, 12 ,12)
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        
+        # 第三次卷积、池化
+        self.conv3 = nn.Sequential(
+            # input:(bitch_size, 128, 12, 12), output:(bitch_size, 256, 12, 12), (12-3+2*1)/1+1 = 12
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=256),
+            nn.RReLU(inplace=True),
+            # output:(bitch_size, 256, 6 ,6)
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        # 参数初始化
+        self.conv1.apply(gaussian_weights_init)
+        self.conv2.apply(gaussian_weights_init)
+        self.conv3.apply(gaussian_weights_init)
+
+        # 全连接层
+        self.fc = nn.Sequential(
+            nn.Dropout(p=0.2),
+            nn.Linear(in_features=256*6*6, out_features=4096),
+            nn.RReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(in_features=4096, out_features=1024),
+            nn.RReLU(inplace=True),
+            nn.Linear(in_features=1024, out_features=256),
+            nn.RReLU(inplace=True),
+            nn.Linear(in_features=256, out_features=7),
+        )
+
+    # 前向传播
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        # 数据扁平化
+        x = x.view(x.shape[0], -1)
+        y = self.fc(x)
+        return y
